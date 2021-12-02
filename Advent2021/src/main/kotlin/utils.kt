754d87ca66d169d99974cece7b89f75667045241
costalapada <franciscocosta.py@gmail.com>
2021-12-02 13:37:15 +0000

    utils.kt added to repo

---
+++ Advent2021/src/main/kotlin/utils.kt
@@ -0,0 +1,9 @@
+import java.io.File
+import java.util.ArrayList
+
+
+fun <T>File.mapLines(transform: (String) -> T ): List<T> {
+    val result = ArrayList<T>()
+    forEachLine(Charsets.UTF_8) { result.add(transform(it)); }
+    return result
+}
